e195ba41421a8b71c1c1b7b6a67669543c5e1bd6
ltta18 <thuylinh7720@gmail.com>
2020-08-02 23:41:42 +0700

    Handle reach end + fail api call + button pressed

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, SafeAreaView, FlatList } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  ActivityIndicator, 
+  SafeAreaView, 
+  FlatList,
+  Linking } from 'react-native';
 import moment from 'moment';
 import { Card, Button, Icon } from 'react-native-elements';
 
@@ -8,23 +15,49 @@ export default function App() {
   const [loading, setLoading] = useState(true);
   const [articles, setArticles] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
+  const [hasErrored, setHasApiError] = useState(false);
+  const [lastPageReached, setLastPageReached] = useState(false);
 
   const getNews = async () => {
-    const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=6eec2f7fe6cd4c40a3fef8f33f5778fe&page=${pageNumber}`
-    );
-    const jsonData = await response.json();
-    const newArticleList = filterForUniqueArticles(
-      articles.concat(jsonData.articles)
-    );
-    setArticles(newArticleList);
-    setPageNumber(pageNumber + 1);
+    if (lastPageReached) return;
+
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=6eec2f7fe6cd4c40a3fef8f33f5778fe&page=${pageNumber}`
+      );
+      const jsonData = await response.json();
+      const hasMoreArticles = jsonData.articles.length > 0;
+      if (hasMoreArticles) {
+        const newArticleList = filterForUniqueArticles(
+          articles.concat(jsonData.articles)
+        );
+        setArticles(newArticleList);
+        setPageNumber(pageNumber + 1);
+      } else {
+        setLastPageReached(true);
+      }
+    } catch (error) {
+      setHasApiError(true);
+    }
     setLoading(false);
+  };
+
+  
+
+  const handlePress = url => {
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log(`Don't know how to open URL: ${url}`);
+      }
+    });
   };
 
   useEffect(() => {
     getNews()
-  }, [])
+  }, [articles])
 
   const renderArticleItem = ({ item }) => {
     return (
@@ -40,7 +73,12 @@ export default function App() {
             {moment(item.publishedAt).format('LLL')}
           </Text>
         </View>
-        <Button icon={<Icon />} title="Read more" backgroundColor="#03A9F4" />
+        <Button 
+          icon={<Icon />} 
+          title="Read more" 
+          backgroundColor="#03A9F4"
+          onPress={() => handlePress(item.url)}
+        />
       </Card>
     );
   };
@@ -68,6 +106,15 @@ export default function App() {
       </View>
     );
   }
+
+  if (hasErrored) {
+    return (
+      <View style={styles.container}>
+        <Text>Error =(</Text>
+      </View>
+    );
+  }
+  
   return (
     <SafeAreaView style={styles.safeView}>
       <View style={styles.container}>
@@ -81,6 +128,10 @@ export default function App() {
           keyExtractor={item => item.title}
           onEndReached={getNews} 
           onEndReachedThreshold={1}
+          ListFooterComponent={lastPageReached ? <Text>No more articles</Text> : <ActivityIndicator
+            size="large"
+            loading={loading}
+          />}
         />
       </View>
     </SafeAreaView>
