ee19d7e4d50c894a2a619363c7262e1a55940360
ltta18 <thuylinh7720@gmail.com>
2020-08-02 15:31:12 +0700

    Add api request

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+
+  const getNews = async () => {
+    const response = await fetch(
+      'https://newsapi.org/v2/top-headlines?country=us&apiKey=d05ce933a10240f18baec1e2cb2da323'
+    );
+    const jsonData = await response.json();
+    setArticles(jsonData.articles);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getNews()
+  }, [])
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator 
+          size="large" 
+          loading={loading} 
+        />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
     </View>
-  );
+  )
+  
 }
 
 const styles = StyleSheet.create({
